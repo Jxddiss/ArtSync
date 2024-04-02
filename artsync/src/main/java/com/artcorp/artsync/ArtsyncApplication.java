c1cd5ce94c8aab84f636940a02c84825a3153512
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-04-01 21:08:05 -0400

    initial commit

---
+++ artsync/src/main/java/com/artcorp/artsync/ArtsyncApplication.java
@@ -0,0 +1,13 @@
+package com.artcorp.artsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtsyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtsyncApplication.class, args);
+    }
+
+}
---
+++ artsync/src/test/java/com/artcorp/artsync/ArtsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artcorp.artsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtsyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

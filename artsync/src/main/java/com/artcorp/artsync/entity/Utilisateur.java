202cbc14ec34672785f3be226d9d812e2755ae84
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-04-03 12:37:15 -0400

    Entity Utilisateur commencé

--- artsync/src/main/java/com/artcorp/artsync/entity/Utilisateur.java
+++ artsync/src/main/java/com/artcorp/artsync/entity/Utilisateur.java
@@ -1,8 +1,19 @@
 package com.artcorp.artsync.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class Utilisateur {
 
+    @Id
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }

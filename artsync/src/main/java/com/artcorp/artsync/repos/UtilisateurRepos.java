8225ca34a75746d1717aa4d131b8df30c21665ab
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-04-03 14:11:46 -0400

    Commentaire et post entity

--- artsync/src/main/java/com/artcorp/artsync/repos/UtilisateurRepos.java
+++ artsync/src/main/java/com/artcorp/artsync/repos/UtilisateurRepos.java
@@ -4,6 +4,8 @@ import com.artcorp.artsync.entity.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UtilisateurRepos extends JpaRepository<Utilisateur, Long> {
 

d776d0af1fa428226bb73d77eca2540834fe9e68
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-04-14 17:15:29 -0400

    Ajout appel audio

--- artsync/src/main/resources/static/js/chat-video.js
+++ artsync/src/main/resources/static/js/chat-video.js
@@ -25,106 +25,122 @@ document.addEventListener("DOMContentLoaded", function() {
         if (navigator.mediaDevices.getUserMedia) {
             navigator.mediaDevices.getUserMedia({ video: true,audio: false })
                 .then(stream => {
-                    localStream = stream;
-                    const socketVideo = new SockJS('/websocket');
-                    stompClientVideo = Stomp.over(socketVideo);
-                    stompClientVideo.connect({}, function(frame) {
-                        stompClientVideo.subscribe('/topic/appel/call/'+conversationId+"/"+idUser, (call) => {
-                            console.log("appel de : " + call.body);
-                            localPeer.ontrack = (event) => {
-                                remoteVideo.srcObject = event.streams[0];
-                            }
-
-                            localPeer.onicecandidate = (event) => {
-                                if (event.candidate) {
-                                    var candidate = {
-                                        type: 'candidate',
-                                        label: event.candidate.sdpMLineIndex,
-                                        id: event.candidate.candidate,
-                                    }
-                                    console.log("sending candidate : " + JSON.stringify(candidate));
-                                    stompClientVideo.send('/app/chat/appel/candidate/'+conversationId+"/"+idAmi, {}, JSON.stringify({
-                                        toUser: idAmi,
-                                        fromUser: idUser,
-                                        candidate: candidate
-                                    }));
-                                }
-                            }
-
-                            localStream.getTracks().forEach(track => localPeer.addTrack(track, localStream));
-
-                            localPeer.createOffer().then((description) => {
-                                localPeer.setLocalDescription(description);
-                                console.log("Setting local description : " + JSON.stringify(description));
-                                stompClientVideo.send('/app/chat/appel/offer/'+conversationId+"/"+idAmi, {}, JSON.stringify({
-                                    toUser: idAmi,
-                                    fromUser: idUser,
-                                    offer: description
-                                }))
-                            })
-                        });
-
-                        stompClientVideo.subscribe('/topic/appel/offer/'+conversationId+"/"+idUser, (offer) => {
-                            console.log("offer de : " + offer.body);
-                            var offer = JSON.parse(offer.body)["offer"];
-                            localPeer.ontrack = (event) => {
-                                remoteVideo.srcObject = event.streams[0];
-                            }
-                            localPeer.onicecandidate = (event) => {
-                                if (event.candidate) {
-                                    var candidate = {
-                                        type: 'candidate',
-                                        label: event.candidate.sdpMLineIndex,
-                                        id: event.candidate.candidate,
-                                    }
-                                    console.log("sending candidate : " + JSON.stringify(candidate));
-                                    stompClientVideo.send('/app/chat/appel/candidate/'+conversationId+"/"+idAmi, {}, JSON.stringify({
-                                        toUser: idAmi,
-                                        fromUser: idUser,
-                                        candidate: candidate
-                                    }));
-                                }
-                            }
-
-                            localStream.getTracks().forEach(track => localPeer.addTrack(track, localStream));
-
-                            localPeer.setRemoteDescription(new RTCSessionDescription(offer));
-                            localPeer.createAnswer().then((description) => {
-                                localPeer.setLocalDescription(description);
-                                console.log("Setting local description : " + JSON.stringify(description));
-                                stompClientVideo.send('/app/chat/appel/answer/'+conversationId+"/"+idAmi, {}, JSON.stringify({
-                                    toUser: idAmi,
-                                    fromUser: idUser,
-                                    answer: description
-                                }))
-                            })
-                        })
-
-                        stompClientVideo.subscribe('/topic/appel/answer/'+conversationId+"/"+idUser, (answer) => {
-                            console.log("answer de : " + answer.body);
-                            var answerO = JSON.parse(answer.body)["answer"];
-                            localPeer.setRemoteDescription(new RTCSessionDescription(answerO));
-                        })
-
-                        stompClientVideo.subscribe('/topic/appel/candidate/'+conversationId+"/"+idUser, (candidate) => {
-                            console.log("candidate de : " + candidate.body);
-                            var candidateO = JSON.parse(candidate.body)["candidate"];
-                            var iceCandidate = new RTCIceCandidate({
-                                sdpMLineIndex: candidateO["label"],
-                                candidate: candidateO["id"]
-                            });
-                            localPeer.addIceCandidate(iceCandidate);
-                            console.log("added candidate : " + JSON.stringify(candidateO));
-                        })
-
-                        stompClientVideo.send("/app/chat/appel/add/"+conversationId, {}, idUser)
-                        stompClientVideo.send("/app/chat/appel/call/"+conversationId+"/"+idAmi, {}, idUser)
-                    })
-                    localVideo.srcObject = stream;
+                    call(stream)
                 })
                 .catch(function (error) {
                     console.log("Something went wrong! : " + error);
                 });
         }
     });
+    phone.addEventListener("click", function() {
+        videoDialog.showModal();
+        if (navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices.getUserMedia({ video: false,audio: true })
+                .then(stream => {
+                    call(stream)
+                })
+                .catch(function (error) {
+                    console.log("Something went wrong! : " + error);
+                });
+        }
+    });
+    function call(stream){
+        localStream = stream;
+        const socketVideo = new SockJS('/websocket');
+        stompClientVideo = Stomp.over(socketVideo);
+        stompClientVideo.connect({}, function(frame) {
+            stompClientVideo.subscribe('/topic/appel/call/'+conversationId+"/"+idUser, (call) => {
+                console.log("appel de : " + call.body);
+                localPeer.ontrack = (event) => {
+                    remoteVideo.srcObject = event.streams[0];
+                }
+
+                localPeer.onicecandidate = (event) => {
+                    if (event.candidate) {
+                        var candidate = {
+                            type: 'candidate',
+                            label: event.candidate.sdpMLineIndex,
+                            id: event.candidate.candidate,
+                        }
+                        console.log("sending candidate : " + JSON.stringify(candidate));
+                        stompClientVideo.send('/app/chat/appel/candidate/'+conversationId+"/"+idAmi, {}, JSON.stringify({
+                            toUser: idAmi,
+                            fromUser: idUser,
+                            candidate: candidate
+                        }));
+                    }
+                }
+
+                localStream.getTracks().forEach(track => localPeer.addTrack(track, localStream));
+
+                localPeer.createOffer().then((description) => {
+                    localPeer.setLocalDescription(description);
+                    console.log("Setting local description : " + JSON.stringify(description));
+                    stompClientVideo.send('/app/chat/appel/offer/'+conversationId+"/"+idAmi, {}, JSON.stringify({
+                        toUser: idAmi,
+                        fromUser: idUser,
+                        offer: description
+                    }))
+                })
+            });
+
+            stompClientVideo.subscribe('/topic/appel/offer/'+conversationId+"/"+idUser, (offer) => {
+                console.log("offer de : " + offer.body);
+                var offer = JSON.parse(offer.body)["offer"];
+                localPeer.ontrack = (event) => {
+                    remoteVideo.srcObject = event.streams[0];
+                }
+                localPeer.onicecandidate = (event) => {
+                    if (event.candidate) {
+                        var candidate = {
+                            type: 'candidate',
+                            label: event.candidate.sdpMLineIndex,
+                            id: event.candidate.candidate,
+                        }
+                        console.log("sending candidate : " + JSON.stringify(candidate));
+                        stompClientVideo.send('/app/chat/appel/candidate/'+conversationId+"/"+idAmi, {}, JSON.stringify({
+                            toUser: idAmi,
+                            fromUser: idUser,
+                            candidate: candidate
+                        }));
+                    }
+                }
+
+                localStream.getTracks().forEach(track => localPeer.addTrack(track, localStream));
+
+                localPeer.setRemoteDescription(new RTCSessionDescription(offer));
+                localPeer.createAnswer().then((description) => {
+                    localPeer.setLocalDescription(description);
+                    console.log("Setting local description : " + JSON.stringify(description));
+                    stompClientVideo.send('/app/chat/appel/answer/'+conversationId+"/"+idAmi, {}, JSON.stringify({
+                        toUser: idAmi,
+                        fromUser: idUser,
+                        answer: description
+                    }))
+                })
+            })
+
+            stompClientVideo.subscribe('/topic/appel/answer/'+conversationId+"/"+idUser, (answer) => {
+                console.log("answer de : " + answer.body);
+                var answerO = JSON.parse(answer.body)["answer"];
+                localPeer.setRemoteDescription(new RTCSessionDescription(answerO));
+            })
+
+            stompClientVideo.subscribe('/topic/appel/candidate/'+conversationId+"/"+idUser, (candidate) => {
+                console.log("candidate de : " + candidate.body);
+                var candidateO = JSON.parse(candidate.body)["candidate"];
+                var iceCandidate = new RTCIceCandidate({
+                    sdpMLineIndex: candidateO["label"],
+                    candidate: candidateO["id"]
+                });
+                localPeer.addIceCandidate(iceCandidate);
+                console.log("added candidate : " + JSON.stringify(candidateO));
+            })
+
+            stompClientVideo.send("/app/chat/appel/add/"+conversationId, {}, idUser)
+            stompClientVideo.send("/app/chat/appel/call/"+conversationId+"/"+idAmi, {}, idUser)
+        })
+        localVideo.srcObject = stream;
+    }
 })
+

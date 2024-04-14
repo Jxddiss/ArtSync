d74bfc876c59369dd9d004179e898022207cfec5
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-04-14 01:52:39 -0400

    Delete test chat avec file (chatWebsockest)

--- artsync/src/main/java/com/artcorp/artsync/controller/ChatController.java
+++ artsync/src/main/java/com/artcorp/artsync/controller/ChatController.java
@@ -1,32 +1,13 @@
 package com.artcorp.artsync.controller;
 
 import com.artcorp.artsync.entity.Chat;
-import com.artcorp.artsync.dto.ChatWebSocket;
-import com.artcorp.artsync.entity.FichierGeneral;
-import com.artcorp.artsync.entity.Post;
 import com.artcorp.artsync.service.ChatService;
 import com.artcorp.artsync.service.impl.ChatServiceImpl;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.time.LocalDateTime;
-import java.util.HashSet;
-
-import static com.artcorp.artsync.constant.FileConstant.MEDIA_CHAT_BASE_FOLDER;
 import static com.artcorp.artsync.constant.FileConstant.RELATIVE_PATH;
 
 @Controller

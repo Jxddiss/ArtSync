60bf5bdbaf5f757cfada0c4d1b2e0c72e48b4db4
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-04-14 01:20:54 -0400

    Gestion des fichier chat media(image pour l'instant)

--- artsync/src/main/java/com/artcorp/artsync/constant/FileConstant.java
+++ artsync/src/main/java/com/artcorp/artsync/constant/FileConstant.java
@@ -3,6 +3,6 @@ package com.artcorp.artsync.constant;
 public class FileConstant {
     public static final String USER_FOLDER = "src\\main\\resources\\static\\media\\image\\";
     public static final String DEFAULT_USER_IMAGE = "default.png";
-
     public static final String MEDIA_CHAT_BASE_FOLDER = "src\\main\\resources\\static\\media\\chat\\";
+    public static final String RELATIVE_PATH = "/media/chat/";
 }
--- artsync/src/main/java/com/artcorp/artsync/controller/ChatController.java
+++ artsync/src/main/java/com/artcorp/artsync/controller/ChatController.java
@@ -6,20 +6,28 @@ import com.artcorp.artsync.entity.FichierGeneral;
 import com.artcorp.artsync.entity.Post;
 import com.artcorp.artsync.service.ChatService;
 import com.artcorp.artsync.service.impl.ChatServiceImpl;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 
 import static com.artcorp.artsync.constant.FileConstant.MEDIA_CHAT_BASE_FOLDER;
+import static com.artcorp.artsync.constant.FileConstant.RELATIVE_PATH;
 
 @Controller
 public class ChatController {
@@ -32,22 +40,15 @@ public class ChatController {
 
     @MessageMapping("/chat/{conversationId}")
     @SendTo("/topic/conversation/{conversationId}")
-    public Chat sendMessage(ChatWebSocket chatWS, SimpMessageHeaderAccessor headerAccessor, String conversationId) throws IOException {
-        Chat chat = chatWS.getChat();
-        MultipartFile media = chatWS.getMedia();
-        if (media != null){
-            String originalFilename = StringUtils.cleanPath(media.getOriginalFilename());
-
-            File parentDir = new File(MEDIA_CHAT_BASE_FOLDER);
-            File saveFile = new File(parentDir.getAbsolutePath() + File.separator + conversationId + File.separator + originalFilename);
-            saveFile.mkdirs();
-            media.transferTo(saveFile);
-            chat.setUrlMedia(saveFile.getAbsolutePath());
+    public Chat sendMessage(Chat chat) {
+        if (!StringUtils.isEmpty(chat.getUrlMedia())) {
+            chat.setUrlMedia(RELATIVE_PATH  + chat.getUrlMedia());
         }
-        System.out.println(chat);
         chatService.save(chat);
         return chat;
     }
+
+
 }
 
 

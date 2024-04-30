386a59de67bbdea8309fbf804eceda1ac1ef82d2
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-04-30 14:40:22 -0400

    Notification de groupe ajoutée

--- artsync/src/main/java/com/artcorp/artsync/controller/websocket/NotificationController.java
+++ artsync/src/main/java/com/artcorp/artsync/controller/websocket/NotificationController.java
@@ -1,16 +1,45 @@
 package com.artcorp.artsync.controller.websocket;
 
 import com.artcorp.artsync.entity.Notification;
+import com.artcorp.artsync.entity.Projet;
+import com.artcorp.artsync.entity.Utilisateur;
+import com.artcorp.artsync.service.ProjetService;
+import com.artcorp.artsync.service.impl.ProjetServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+
+import java.util.List;
 
 @Controller
 public class NotificationController {
+    private SimpMessagingTemplate simpMessagingTemplate;
+    private ProjetService projetService;
+
+    @Autowired
+    public NotificationController(SimpMessagingTemplate simpMessagingTemplate, ProjetServiceImpl projetService) {
+        this.simpMessagingTemplate = simpMessagingTemplate;
+        this.projetService = projetService;
+    }
 
     @MessageMapping("/notification/{userId}")
     @SendTo("/topic/notification/{userId}")
     public Notification notificationVise(Notification notif){
+        return notif;
+    }
+
+    @MessageMapping("/notification/projet/{projetId}")
+    @SendTo("/topic/notification/projet/{projetId}")
+    public Notification notificationProjet(Notification notif, @DestinationVariable("projetId") Long projetId){
+        List<Utilisateur> utilisateursProjet = projetService.getMembers(projetId);
+        if (utilisateursProjet != null){
+            utilisateursProjet.forEach(user ->{
+                this.simpMessagingTemplate.convertAndSend("/topic/notification/"+user.getId(), notif);
+            });
+        }
         return notif;
     }
 }

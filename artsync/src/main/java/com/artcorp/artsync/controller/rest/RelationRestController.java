04efa77f1165cbc22a9889e7c193a4922c30d77e
ben4ali <alibenkarrouch@gmail.com>
2024-04-30 00:58:58 -0400

    Ajout d'ajax pour les opérations de groupe

--- artsync/src/main/java/com/artcorp/artsync/controller/rest/RelationRestController.java
+++ artsync/src/main/java/com/artcorp/artsync/controller/rest/RelationRestController.java
@@ -1,9 +1,8 @@
 package com.artcorp.artsync.controller.rest;
 
 
-import com.artcorp.artsync.entity.Commentaire;
-import com.artcorp.artsync.entity.Forum;
-import com.artcorp.artsync.entity.Utilisateur;
+import com.artcorp.artsync.entity.*;
+import com.artcorp.artsync.service.DemandeService;
 import com.artcorp.artsync.service.ProjetService;
 import com.artcorp.artsync.service.UtilisateurService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,9 +18,11 @@ public class RelationRestController {
     UtilisateurService utilisateurService;
     @Autowired
     ProjetService projetService;
+    @Autowired
+    DemandeService demandeService;
 
     @PostMapping("/recherche/updateRelationUsr")
-    public String rechercheUtilisateur(@Param("userId") Long userId, HttpServletRequest request){
+    public String updateRelationUser(@Param("userId") Long userId, HttpServletRequest request){
         HttpSession session = request.getSession(false);
         if (session == null) {
             return "false";
@@ -31,5 +32,29 @@ public class RelationRestController {
         utilisateur = utilisateurService.findById(utilisateur.getId());
         session.setAttribute("user",utilisateur);
         return "true";
+    }
+    @PostMapping("/recherche/updateRelationGrp")
+    public String updateRelationProject (@Param("projetId") Long projetId,@Param("operation") String operation, HttpServletRequest request){
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return "Pas d'utilisateur";
+        }
+        Utilisateur utilisateur = (Utilisateur) session.getAttribute("user");
+        if (operation.equals("add")){
+            if (!projetService.findById(projetId).isPublique()){
+                for (Demande demande:demandeService.findByUtilisateurId(utilisateur.getId())){
+                    if (demande.getProjet().equals(projetService.findById(projetId))){
+                        return "Demande déjà en cours.";
+                    }
+                }
+                demandeService.createDemande(utilisateur.getId(),projetId);
+                return "Demande envoyée";
+            }
+            projetService.addUtilisateurToProjet(projetId,utilisateur.getId());
+            return "Ajouté au groupe";
+        }else{
+            projetService.removeUtilisateurFromProjet(projetId,utilisateur.getId());
+            return "Supprimé du groupe";
+        }
     }
 }

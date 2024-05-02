d73ad2a16df5401c13d9fac268b0adc8f2a3a13d
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-05-02 00:59:01 -0400

    Encryption des mots de passes

--- artsync/src/main/java/com/artcorp/artsync/ArtsyncApplication.java
+++ artsync/src/main/java/com/artcorp/artsync/ArtsyncApplication.java
@@ -2,12 +2,23 @@ package com.artcorp.artsync;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ArtsyncApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ArtsyncApplication.class, args);
+    }
+
+
+
+
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
--- artsync/src/main/java/com/artcorp/artsync/service/UtilisateurService.java
+++ artsync/src/main/java/com/artcorp/artsync/service/UtilisateurService.java
@@ -1,11 +1,12 @@
 package com.artcorp.artsync.service;
 
 import com.artcorp.artsync.entity.Utilisateur;
+import com.artcorp.artsync.exception.domain.MauvaisIdentifiantException;
 
 import java.util.List;
 
 public interface UtilisateurService {
-    Utilisateur connexion(String username, String password);
+    Utilisateur connexion(String username, String password) throws MauvaisIdentifiantException;
     Utilisateur inscription(String pseudo, String prenom, String nom, String email, String password, String photoUrl, String specialisation, String statut);
     List<Utilisateur> findAll();
     Utilisateur findByPseudo(String pseudo);

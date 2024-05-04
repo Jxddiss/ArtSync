9660c27469e566462aee1c21a0baa3f37dbcbf88
ben4ali <alibenkarrouch@gmail.com>
2024-05-04 03:35:33 -0400

    Les pages de groupes sont tous responsives

--- artsync/src/main/resources/static/js/script-group.js
+++ artsync/src/main/resources/static/js/script-group.js
@@ -1,15 +1,15 @@
 const add= document.getElementById('add');
 const inputAnnonce = document.getElementById('inputAnnonce');
 const newfilePreview = document.getElementById('newfilePreview');
-let open = false
+let openAnnonce = false
 if (add){
     add.addEventListener('click', function() {
-        if (!open) {
+        if (!openAnnonce) {
             gsap.to(inputAnnonce, {opacity: 1, width: '100%'});
-            open = true;
+            openAnnonce = true;
         }else {
             gsap.to(inputAnnonce, {opacity: 0, width: '0%'});
-            open = false;
+            openAnnonce = false;
         }
     });
 }
@@ -84,3 +84,108 @@ if (demandeHolder){
     const children = demandeHolder.children
     gsap.to(children, {duration: 0.5, opacity: 1, delay: 0.15, stagger: 0.1, ease:'back', filter: 'blur(0px)'})
 }
+
+const vNavBar = document.querySelector(".verticalNavbar")
+const verticalBtn = document.querySelector(".verticalButton")
+const projectContent = document.querySelector(".projectContent")
+let openNavbar = false
+verticalBtn.addEventListener("click",function (){
+    if (!openNavbar){
+        openNavbar = true
+        gsap.to(vNavBar,{
+            display:"flex"
+        })
+        gsap.to(vNavBar,{
+            width:"100%",
+            opacity:1,
+            duration:0.65,
+            ease:"back"
+        })
+        gsap.to(projectContent,{
+            width:"0%",
+            opacity:0,
+            duration:0.25,
+        })
+        gsap.to(projectContent,{
+            display:"none",
+            delay:0,
+        })
+        gsap.to(verticalBtn,{
+            rotate:180,
+            ease:"back"
+        })
+    }else{
+        openNavbar = false
+        gsap.to(vNavBar,{
+            width:"0%",
+            opacity:0,
+            duration:0.25,
+        })
+        gsap.to(projectContent,{
+            width:"100%",
+            opacity:1,
+            duration:0.65,
+            ease:"back"
+        })
+        gsap.to(verticalBtn,{
+            rotate:0,
+            ease:"back"
+        })
+        gsap.to(projectContent,{
+            display:"flex",
+            delay:0,
+        })
+    }
+})
+
+const annonceBar = document.querySelector(".projectContainer2")
+const annonceBtn = document.querySelector(".annonceBtn")
+let openAnnonceMobile = false
+
+annonceBtn.addEventListener("click",function (){
+    if (!openAnnonceMobile){
+        openAnnonceMobile = true
+        gsap.to(annonceBar,{
+            display:"flex"
+        })
+        gsap.to(annonceBar,{
+            width:"100%",
+            paddingRight:"10%",
+            opacity:1,
+            duration:0.65,
+            ease:"back"
+        })
+        gsap.to(projectContent,{
+            width:"55%",
+            duration:0.25,
+        })
+        gsap.to(projectContent,{
+            delay:0,
+        })
+        gsap.to(annonceBtn,{
+            rotate:180,
+            ease:"back"
+        })
+    }else{
+        openAnnonceMobile = false
+        gsap.to(annonceBar,{
+            width:"0%",
+            opacity:0,
+            duration:0.25,
+        })
+        gsap.to(annonceBar,{
+            display:"none"
+        })
+        gsap.to(projectContent,{
+            width:"100%",
+            opacity:1,
+            duration:0.65,
+            ease:"back"
+        })
+        gsap.to(annonceBtn,{
+            rotate:0,
+            ease:"back"
+        })
+    }
+    console.log(openAnnonceMobile)
+})

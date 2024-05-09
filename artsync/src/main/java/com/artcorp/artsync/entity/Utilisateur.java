a97e31ae5705487bd90a2d301e87022b88efdd6f
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-05-08 23:51:53 -0400

    fetch type user

--- artsync/src/main/java/com/artcorp/artsync/entity/Utilisateur.java
+++ artsync/src/main/java/com/artcorp/artsync/entity/Utilisateur.java
@@ -29,7 +29,7 @@ public class Utilisateur {
     private String statut;
     private String backgroundColor;
     private String backgroundTexture;
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany
     @JoinTable(name="utilisateurs_relation",
             joinColumns = @JoinColumn(name = "utilisateur_un_id"),
             inverseJoinColumns = @JoinColumn(name = "utilisateur_deux_id"),
@@ -37,7 +37,7 @@ public class Utilisateur {
     )
     private Set<Utilisateur> followers;
 
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany
     @JoinTable(name="utilisateurs_relation",
             joinColumns = @JoinColumn(name = "utilisateur_deux_id"),
             inverseJoinColumns = @JoinColumn(name = "utilisateur_un_id"),

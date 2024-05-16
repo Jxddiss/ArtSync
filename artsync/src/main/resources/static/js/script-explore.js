2fbfe5586dcad386ef4a8b5e3bc54c38d7348df6
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-05-16 14:15:18 -0400

    Ajout like a explorer

--- artsync/src/main/resources/static/js/script-explore.js
+++ artsync/src/main/resources/static/js/script-explore.js
@@ -21,8 +21,8 @@ document.addEventListener("DOMContentLoaded", function () {
             imgDialog.src = imageUrl;
             let cardPostDialog = cardPost.cloneNode(true);
             cardPostDialog.style.display = "flex";
+            handleLike(cardPostDialog);
             dialog.querySelector(".poste").appendChild(cardPostDialog)
-
             dialog.showModal();
         })
 
@@ -117,3 +117,94 @@ function ajouterCommentaire(form){
 document.addEventListener("submit", event =>{
     event.preventDefault();
 })
+
+function handleLike(cardpost){
+    const likeSymbol = cardpost.querySelector(".like")
+    const postId = likeSymbol.getAttribute("post-id");
+    const ownerId = likeSymbol.getAttribute("owner-id")
+    const icon = likeSymbol.querySelector("i");
+
+    if(localStorage.getItem(`like-post-${postId}-${userId}`)){
+        if(localStorage.getItem(`like-post-${postId}-${userId}`) === "true"){
+            likeSymbol.dataset.clicked = "true";
+            icon.style.color = "red";
+            let fill = icon.classList[1] + "-fill";
+            icon.classList.remove(icon.classList[1]);
+            icon.classList.add(fill);
+        }else{
+            likeSymbol.dataset.clicked = "false";
+        }
+    }else{
+        localStorage.setItem(`like-post-${postId}-${userId}`,`false`);
+    }
+
+    likeSymbol.firstElementChild.addEventListener("mouseout", (e) => {
+        if (likeSymbol.dataset.clicked === "false") {
+            let newIconClass = icon.classList[1].replace("-fill", "");
+            icon.classList.remove(icon.classList[1]);
+            icon.classList.add(newIconClass);
+        }
+    });
+
+    likeSymbol.firstElementChild.addEventListener("mouseover", (e) => {
+        if (likeSymbol.dataset.clicked === "false") {
+            let fill = icon.classList[1] + "-fill";
+            icon.classList.remove(icon.classList[1]);
+            icon.classList.add(fill);
+        }
+    });
+
+    likeSymbol.firstElementChild.addEventListener("click", (e) => {
+        const info = likeSymbol.querySelector("p").innerText
+        let nbLike = +info.split(" ").at(0);
+
+        if (likeSymbol.dataset.clicked === "false") {
+            likeSymbol.dataset.clicked = "true";
+            icon.style.color = "red";
+            likePost("like",postId,ownerId);
+            nbLike++
+            likeSymbol.querySelector("p").innerText = `${nbLike} J'aimes`;
+            localStorage.setItem(`like-post-${postId}-${userId}`,`true`);
+        } else {
+            likePost("unlike", postId,ownerId);
+            let newIconClass = icon.classList[1].replace("-fill", "");
+            icon.classList.remove(icon.classList[1]);
+            icon.classList.add(newIconClass);
+            likeSymbol.dataset.clicked = "false";
+            icon.style.color = "black";
+            nbLike--
+            likeSymbol.querySelector("p").innerText = `${nbLike} J'aimes`;
+            localStorage.setItem(`like-post-${postId}`,`false`);
+        }
+    });
+}
+
+function likePost(type, postId,idPostOwner){
+    $.ajax({
+        type: "POST",
+        url: window.location.origin.toString()+"/post/like",
+        data: {like: type, postId: postId},
+        success : function (data) {
+            if(data === "true"){
+                console.log("PASSED")
+            }else{
+                console.log("FAILED")
+            }
+        },
+    })
+
+    if (userId !== idPostOwner){
+        if(type === "like"){
+            stompClientNotif.send("/app/notification/"+idPostOwner,{},JSON.stringify(
+                {
+                    type: 'info',
+                    pseudoSender: pseudoUser,
+                    message: `Nouvelle mention jaime de ${pseudoUser}` ,
+                    titre: 'Nouveau like',
+                    imgSender: userImage,
+                    urlNotif: window.location.origin.toString() + '/feed'
+                }
+            ));
+        }
+    }
+}

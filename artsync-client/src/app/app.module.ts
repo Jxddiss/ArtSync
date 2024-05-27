1453905f36c8bdbe8162aa3ef513761747f32f21
ben4ali <alibenkarrouch@gmail.com>
2024-05-27 12:32:02 -0400

    Ajout du vertical header

--- artsync-client/src/app/app.module.ts
+++ artsync-client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OptionsComponent } from './options/options.component';
+import { GeneralViewComponent } from './general-view/general-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OptionsComponent,
+    GeneralViewComponent
   ],
   imports: [
     BrowserModule,
---
+++ artsync-client/src/assets/js/script.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const verticalNavbar = document.querySelector('.vertical-header');
+    const options = document.querySelectorAll('.option');
+    const closeBtn = options.item(0).firstElementChild;
+
+    let menuOpen = true;
+    closeBtn.addEventListener('click', function() {
+        if (menuOpen) {
+            menuOpen = false;
+            options.forEach(option => {
+                gsap.to(option.lastElementChild, {
+                    opacity: 0,
+                    duration: 0.2,
+                    onComplete: () =>{
+                        option.lastElementChild.style.display = "none";
+                    }
+                });
+            });
+            gsap.to(verticalNavbar,{
+                borderRadius: "0px 15px 0px 0px",
+                width:"3rem",
+                ease: "sine",
+                delay: 0.2
+            })
+        }
+        else{
+            menuOpen = true;
+            options.forEach(option => {
+                option.lastElementChild.style.display = "block";
+                gsap.to(option.lastElementChild, {
+                    opacity: 1,
+                    duration: 1,
+                    delay: 0.5
+                });
+            });
+            gsap.to(verticalNavbar,{
+                borderRadius: "0px 60px 0px 0;x",
+                width:"20rem",
+                ease: "sine",
+            })
+        }
+    });
+});

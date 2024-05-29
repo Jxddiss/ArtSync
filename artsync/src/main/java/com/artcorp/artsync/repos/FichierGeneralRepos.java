2094754a2c8b1bf37ca0c4572f9ed8562ea99456
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-05-29 01:17:29 -0400

    get fichiers par users

--- artsync/src/main/java/com/artcorp/artsync/repos/FichierGeneralRepos.java
+++ artsync/src/main/java/com/artcorp/artsync/repos/FichierGeneralRepos.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Repository
 public interface FichierGeneralRepos extends JpaRepository<FichierGeneral, Long> {
     List<FichierGeneral> findAllByProjet(Projet projet);
-    @Query("select f from FichierGeneral f where f.projet != null and f.utilisateur = ?1")
+    @Query("select f from FichierGeneral f where f.utilisateur = ?1 AND f.projet IS NOT NULL ")
     List<FichierGeneral> findAllByUtilisateur(Utilisateur utilisateur);
     List<FichierGeneral> findAllByProjetAndUtilisateur(Projet projet, Utilisateur utilisateur);
     Long countByProjet(Projet projet);

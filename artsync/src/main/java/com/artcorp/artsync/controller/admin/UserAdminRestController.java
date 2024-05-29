771472bced1760f57c7c303a1ad71fb1f1528dfb
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-05-29 00:25:04 -0400

    Ajout get les forums par utilisateurs

--- artsync/src/main/java/com/artcorp/artsync/controller/admin/UserAdminRestController.java
+++ artsync/src/main/java/com/artcorp/artsync/controller/admin/UserAdminRestController.java
@@ -1,14 +1,8 @@
 package com.artcorp.artsync.controller.admin;
 
 import com.artcorp.artsync.dto.HttpResponse;
-import com.artcorp.artsync.entity.Commentaire;
-import com.artcorp.artsync.entity.Post;
-import com.artcorp.artsync.entity.Projet;
-import com.artcorp.artsync.entity.Utilisateur;
-import com.artcorp.artsync.service.CommentaireService;
-import com.artcorp.artsync.service.PostService;
-import com.artcorp.artsync.service.ProjetService;
-import com.artcorp.artsync.service.UtilisateurService;
+import com.artcorp.artsync.entity.*;
+import com.artcorp.artsync.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -26,13 +20,15 @@ public class UserAdminRestController {
     private final CommentaireService commentaireService;
     private final ProjetService projetService;
     private final PostService postService;
+    private final ForumService forumService;
 
     @Autowired
-    public UserAdminRestController(UtilisateurService utilisateurService, CommentaireService commentaireService, ProjetService projetService, PostService postService) {
+    public UserAdminRestController(UtilisateurService utilisateurService, CommentaireService commentaireService, ProjetService projetService, PostService postService, ForumService forumService) {
         this.utilisateurService = utilisateurService;
         this.commentaireService = commentaireService;
         this.projetService = projetService;
         this.postService = postService;
+        this.forumService = forumService;
     }
 
     @GetMapping("/api/users")
@@ -75,6 +71,17 @@ public class UserAdminRestController {
             throw new NoResourceFoundException(method,"Utilisateur id: "+userId);
         }
         List<Post> listPost = postService.findPostByUser(utilisateur);
+        return new ResponseEntity<>(listPost,HttpStatus.OK);
+    }
+
+    @GetMapping("/api/users/forums/{userId}")
+    public ResponseEntity<List<Forum>> getForumsByOneUser(@PathVariable("userId") Long userId,
+                                                         HttpMethod method) throws NoResourceFoundException {
+        Utilisateur utilisateur = utilisateurService.findById(userId);
+        if (utilisateur == null){
+            throw new NoResourceFoundException(method,"Utilisateur id: "+userId);
+        }
+        List<Forum> listPost = forumService.findAllByUtilisateur(utilisateur);
         return new ResponseEntity<>(listPost,HttpStatus.OK);
     }
 

3a7fd1b170c3ec3ce4d624dfa26ae827acedf1a2
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-05-29 14:26:40 -0400

    deconnexion admin ajouté

--- artsync-client/src/app/options/options.component.ts
+++ artsync-client/src/app/options/options.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from '../service/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-options',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './options.component.css'
 })
 export class OptionsComponent {
+    constructor(private authenticationService : AuthenticationService, private router : Router){}
 
+    onLogout(){
+      this.authenticationService.logOut();
+      this.router.navigate(['/login']);
+    }
 }

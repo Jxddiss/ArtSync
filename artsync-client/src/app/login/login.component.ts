49a2b62d8e14d8f3d41405b5f6950135a922895c
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-05-28 21:11:24 -0400

    login avec le backend fait

--- artsync-client/src/app/login/login.component.ts
+++ artsync-client/src/app/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../service/authentication.service';
+import { Utilisateur } from '../models/utilisateur.model';
+import { Subscription } from 'rxjs';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -8,6 +11,8 @@ import { AuthenticationService } from '../service/authentication.service';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  private subscriptions: Subscription[] = [];
+  
   constructor(private router: Router, 
     private authenticationService: AuthenticationService) { }
 
@@ -17,5 +22,26 @@ export class LoginComponent {
     }else{
       this.router.navigateByUrl('/login');
     }
+  }
+
+  onLogin(user : Utilisateur): void {
+    console.log(user);
+    this.subscriptions.push(
+      this.authenticationService.login(user).subscribe({
+        next: (response: HttpResponse<Utilisateur>) => {
+          const token = response.headers.get("Jwt-Token");
+          this.authenticationService.saveToken(token!);
+          this.authenticationService.addUserToLocalCache(response.body!);
+          this.router.navigateByUrl('/generalView');
+        },
+        error: (errorResponse: HttpErrorResponse) => {
+          console.log(errorResponse);
+        }
+      })
+    )
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
   }
 }
--- artsync-client/src/app/service/authentication.service.ts
+++ artsync-client/src/app/service/authentication.service.ts
@@ -1,13 +1,63 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../constants/environment.constant';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Utilisateur } from '../models/utilisateur.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  private backend = environment.apiUrl
+  private token: string | null = null
+  private jwtHelper = new JwtHelperService();
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+
+  login(user : Utilisateur) : Observable<HttpResponse<Utilisateur>>{
+    return this.httpClient.post<Utilisateur>(`${this.backend}/api/admin/login`,user,{observe: 'response'});
+  }
+
+  logOut() : void{
+    this.token = null;
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+  }
+
+  saveToken(token : string) : void{
+    this.token = token;
+    localStorage.setItem('token', token);
+  }
 
   isLoggedIn():boolean{
-    return true;
+    this.loadToken();
+    if(this.token != null && this.token !== ''){
+      if(this.jwtHelper.decodeToken(this.token).sub != null || '' || undefined){
+        if(!this.jwtHelper.isTokenExpired(this.token)){
+          return true;
+        }
+      }
+    }else{
+      this.logOut();
+      return false;
+    }
+    return false;
+  }
+
+  addUserToLocalCache(user : Utilisateur) : void{
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+
+  getUserFromLocalCache() : Utilisateur{
+    return JSON.parse(localStorage.getItem('user')!);
+  }
+
+  loadToken() : void{
+    this.token = localStorage.getItem('token');
+  }
+
+  getToken() : string | null{
+    return this.token;
   }
 }

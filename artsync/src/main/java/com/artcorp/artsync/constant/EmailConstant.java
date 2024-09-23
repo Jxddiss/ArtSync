6dfda5c8b087f24923403fcef7e75a19ad8fe420
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-09-22 22:15:02 -0400

    config email

---
+++ artsync/src/main/java/com/artcorp/artsync/constant/EmailConstant.java
@@ -0,0 +1,18 @@
+package com.artcorp.artsync.constant;
+
+public class EmailConstant {
+    public static final String SIMPLE_MAIL_PROTOCOL = "smtps";
+    public static final String USERNAME = "{YOUR_EMAIL}";
+    public static final String PASSWORD = "{YOUR_PASSWORD}";
+    public static final String FROM_EMAIL = "{YOUR_EMAIL}";
+    public static final String CC_EMAIL = "";
+    public static final String EMAIL_SUBJECT = "Lien de rénitialisation de mot de passe";
+    public static final String GMAIL_SMTP_SERVER = "smtp.gmail.com";
+    public static final String SMTP_HOST = "mail.smtp.host";
+    public static final String SMTP_AUTH = "mail.smtp.auth";
+    public static final String SMTP_PORT = "mail.smtp.port";
+    public static final int DEFAULT_PORT = 465;
+    public static final String SMTP_STARTTLS_ENABLE = "mail.smtp.starttls.enable";
+    public static final String SMTP_STARTTLS_REQUIRED = "mail.smtp.starttls.required";
+    public static final String BASE_SITE_ADDRESS = "https://localhost:8443/change-password?token=";
+}
